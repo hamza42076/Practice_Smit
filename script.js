923b25f113b87294d0c5d2cbe28192a7b4aa2872
hamza42076 <164366638+hamza42076@users.noreply.github.com>
2025-07-08 10:49:12 +0500

    first comment

---
+++ script.js
@@ -0,0 +1,9 @@
+let userInut = prompt("Enter your name") ;
+let length = userInut.length;
+for (let i = 0; i < length; i++) {
+    if(userInut.slice(i,i+2) === " "){
+        alert("No double space allowed")
+        break;
+    }
+}
+
